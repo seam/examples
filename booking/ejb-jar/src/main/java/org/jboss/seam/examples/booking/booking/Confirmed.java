8a3657732d7aeb5dbe3b518f5987ddfe9715b0ba
Dan Allen <dan.j.allen@gmail.com>
2009-05-01 19:51:21 +0000

    repackaging

---
+++ booking/ejb-jar/src/main/java/org/jboss/seam/examples/booking/booking/Confirmed.java
@@ -0,0 +1,30 @@
+package org.jboss.seam.examples.booking.booking;
+
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.ElementType.METHOD;
+import static java.lang.annotation.ElementType.PARAMETER;
+import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Inherited;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+import javax.inject.BindingType;
+
+/**
+ * @author Dan Allen
+ */
+public
+@Target(
+{
+   TYPE, METHOD, PARAMETER, FIELD
+})
+@Retention(RUNTIME)
+@Documented
+@BindingType
+@Inherited
+@interface Confirmed
+{
+}
