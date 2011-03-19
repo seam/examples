57fb8a83f2c7d0bd73acc128fdf79a0418211314
Dan Allen <dan.j.allen@gmail.com>
2011-03-19 11:00:53 -0400

    SOLDER-85 update to use repackaged annotations

--- booking/src/main/java/org/jboss/seam/examples/booking/log/BookingLog.java
+++ booking/src/main/java/org/jboss/seam/examples/booking/log/BookingLog.java
@@ -17,29 +17,29 @@
 package org.jboss.seam.examples.booking.log;
 
 import org.jboss.logging.Logger.Level;
-import org.jboss.seam.solder.logging.LogMessage;
-import org.jboss.seam.solder.logging.Message;
+import org.jboss.seam.solder.logging.Log;
 import org.jboss.seam.solder.logging.MessageLogger;
+import org.jboss.seam.solder.messages.Message;
 
 /**
  * @author <a href="http://community.jboss.org/people/dan.j.allen">Dan Allen</a>
  */
 @MessageLogger
 public interface BookingLog {
-    @LogMessage(level = Level.INFO)
+    @Log(level = Level.INFO)
     @Message("%s selected the %s in %s.")
     void hotelSelected(String customerName, String hotelName, String city);
 
-    @LogMessage(level = Level.INFO)
+    @Log(level = Level.INFO)
     @Message("%s initiated a booking at the %s.")
     void bookingInitiated(String customerName, String hotelName);
 
     // QUESTION can positional parameters be used in message string?
-    @LogMessage(level = Level.INFO)
+    @Log(level = Level.INFO)
     @Message("New booking at the %s confirmed for %s.")
     void bookingConfirmed(String hotelName, String customerName);
 
-    @LogMessage(level = Level.INFO)
+    @Log(level = Level.INFO)
     @Message("Does the persistence context still contain the hotel instance? %s")
     void hotelEntityInPersistenceContext(boolean state);
 }
