a455226aabb6227fee3b67472f76dd761774b244
Jozef Hartinger <jharting@redhat.com>
2011-03-26 08:03:58 +0100

    SEAM-62

--- booking/src/test/java/org/jboss/seam/examples/booking/ftest/RegistrationTest.java
+++ booking/src/test/java/org/jboss/seam/examples/booking/ftest/RegistrationTest.java
@@ -1,17 +1,15 @@
 package org.jboss.seam.examples.booking.ftest;
 
-import static org.jboss.test.selenium.guard.request.RequestTypeGuardFactory.waitXhr;
 import static org.jboss.test.selenium.locator.LocatorFactory.jq;
+import static org.testng.AssertJUnit.assertTrue;
 
 import java.text.MessageFormat;
 import java.util.Date;
 
-import org.jboss.test.selenium.dom.Event;
 import org.jboss.test.selenium.locator.JQueryLocator;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-import static org.testng.AssertJUnit.*;
 
 public class RegistrationTest extends AbstractBookingTest
 {
@@ -21,6 +19,7 @@ public class RegistrationTest extends AbstractBookingTest
    public static final JQueryLocator REGISTER_USERNAME_MESSAGE = jq("[id='username:message1']");
    public static final JQueryLocator REGISTER_NAME = jq("[id='name:input']");
    public static final JQueryLocator REGISTER_EMAIL = jq("[id='email:input']");
+   public static final JQueryLocator REGISTER_EMAIL_MESSAGES = jq("[id='email:message1']");
    public static final JQueryLocator REGISTER_PASSWORD = jq("[id='password:input']");
    public static final JQueryLocator REGISTER_PASSWORD_VERIFY = jq("[id='confirmPassword:input']");
    public static final JQueryLocator REGISTER_PASSWORD_VERIFY_MESSAGE = jq("[id='confirmPassword:message1']");
@@ -32,6 +31,9 @@ public class RegistrationTest extends AbstractBookingTest
    public static final String MESSAGE_INCORRECT_PASSWORD = "Passwords do not match. Please re-type the new password.";
    public static final String MESSAGE_USERNAME_SIZE = "size must be between 3 and 15";
    public static final String MESSAGE_PASSWORD_SIZE = "size must be between 5 and 15";
+   public static final String MESSAGE_EMAIL_INCORRECT = "not a well-formed email address";
+   public static final String MESSAGE_USERNAME_DUPLICATE = "The username ''{0}'' is already taken. Please choose another username.";
+   
    private String usernameSuffix;
 
    @BeforeClass
@@ -79,14 +81,26 @@ public class RegistrationTest extends AbstractBookingTest
    public void testPasswordValidation()
    {
       populateRegistrationFields("tester", "tester", "tester@tester.te", "password", "password1");
+      selenium.click(REGISTER_SUBMIT);
+      selenium.waitForPageToLoad();
       assertTrue(selenium.getText(REGISTER_MESSAGES).contains(MESSAGE_INCORRECT_PASSWORD));
+   }
+   
+   @Test
+   public void testEmailValidation()
+   {
+       populateRegistrationFields("tester", "tester", "abcdefgh", "password", "password");
+       selenium.click(REGISTER_SUBMIT);
+       selenium.waitForPageToLoad();
+       assertTrue(selenium.getText(REGISTER_EMAIL_MESSAGES).contains(MESSAGE_EMAIL_INCORRECT));
    }
    
    @Test
    public void testUsernameValidation()
    {
       selenium.type(REGISTER_USERNAME, "A");
-      waitXhr(selenium).fireEvent(REGISTER_USERNAME, Event.BLUR);
+      selenium.click(REGISTER_SUBMIT);
+      selenium.waitForPageToLoad();
       assertTrue(selenium.getText(REGISTER_USERNAME_MESSAGE).contains(MESSAGE_USERNAME_SIZE));
       
       selenium.click(REGISTER_CANCEL);
@@ -95,26 +109,23 @@ public class RegistrationTest extends AbstractBookingTest
       selenium.waitForPageToLoad();
       
       selenium.type(REGISTER_USERNAME, "abcdefghijklmnop");
-      waitXhr(selenium).fireEvent(REGISTER_USERNAME, Event.BLUR);
+      selenium.click(REGISTER_SUBMIT);
+      selenium.waitForPageToLoad();
       assertTrue(selenium.getText(REGISTER_USERNAME_MESSAGE).contains(MESSAGE_USERNAME_SIZE));
+      
+      // test duplicate username
+      populateRegistrationFields("shane", "shane", "shane@example.com", "password", "password");
+      selenium.click(REGISTER_SUBMIT);
+      selenium.waitForPageToLoad();
+      assertTrue(selenium.getText(REGISTER_USERNAME_MESSAGE).contains(MessageFormat.format(MESSAGE_USERNAME_DUPLICATE, "shane")));
    }
 
    protected void populateRegistrationFields(String username, String name, String email, String password, String verify)
    {
       selenium.type(REGISTER_USERNAME, username);
-      waitXhr(selenium).fireEvent(REGISTER_USERNAME, Event.BLUR);
       selenium.type(REGISTER_NAME, name);
       selenium.type(REGISTER_EMAIL, email);
       selenium.type(REGISTER_PASSWORD, password);
       selenium.type(REGISTER_PASSWORD_VERIFY, verify);
-      waitXhr(selenium).fireEvent(REGISTER_PASSWORD_VERIFY, Event.BLUR);
    }
-   
-   /**
-    * TODO: more tests for
-    * - name validation
-    * - email validation
-    * - duplicate user detection
-    */
-
 }
